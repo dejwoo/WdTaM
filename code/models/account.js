05f5323470e5f6bcb9101cfff1b75241c31316c9
Ákos <akoshervay@gmail.com>
2016-11-27 19:25:08 +0100

    Work in sidebar and navbar

--- code/models/account.js
+++ code/models/account.js
@@ -6,6 +6,7 @@ var passportLocalMongoose = require('passport-local-mongoose');
 var Account = new Schema({
     username: String,
     password: String
+
 });
 
 Account.plugin(passportLocalMongoose);
