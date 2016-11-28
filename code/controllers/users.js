fb8e02eb8ab0d105b5dee654fb46702b9f181352
Ákos <akoshervay@gmail.com>
2016-11-28 13:36:21 +0100

    Tweaked the structure

--- code/controllers/users.js
+++ code/controllers/users.js
@@ -1,9 +1,67 @@
-var express = require('express');
-var router = express.Router();
+const async = require('async');
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+const express = require('express');
+const passport = require('passport');
+const Account = require('../models/account');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', function (req, res) {
+    if (req.user) {
+        return res.redirect('/');
+    }
+    res.render('account/login', {user: req.user});
+});
+
+router.post('/login', function (req, res, next) {
+    passport.authenticate('local', function (err, user, info) {
+        //nastala chyba
+        if (err) {
+            return next(err);
+        }
+        //nepodarilo sa authentifikovat
+        if (!user) {
+            return res.render('account/login', {reason: info});
+        }
+        req.logIn(user, function (err) {
+            if (err) {
+                return next(err);
+            }
+            res.redirect('/');
+        });
+    })(req, res, next);
+});
+
+router.get('/register', function (req, res) {
+    res.render('account/register', {});
+});
+
+router.post('/register', function (req, res) {
+    Account.register(new Account({
+        username: req.body.username,
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        isMechanic: req.body.isMechanic
+    }), req.body.password, function (err, account) {
+        if (err) {
+            return res.render('account/register', {account: account});
+        }
+
+        passport.authenticate('local')(req, res, function () {
+            req.session.save(function (err) {
+                if (err) {
+                    return next(err);
+                }
+                res.redirect('/');
+            });
+        });
+    });
+});
+
+router.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
 });
 
 module.exports = router;
