ce23f87134cbade5589f15e92a9901397462e80c
res0lute <jim.sedlacek@gmail.com>
2016-12-06 14:41:47 +0100

    zmeny detail ticket

--- code/public/js/mechanic.js
+++ code/public/js/mechanic.js
@@ -7,12 +7,18 @@ function toggleSide() {
         $('.parent').stop(true,true).animate({
             marginLeft:'0px'
         },300);
+        $('.footer').stop(true,true).animate({
+            paddingLeft:'0px'
+        },300);
     } else {
         $('.left-panel').stop(true,true).animate({
             left:'0px'
         },300).addClass('opened');
         $('.parent').stop(true,true).animate({
             marginLeft:$('.left-panel').width()
+        },300);
+        $('.footer').stop(true,true).animate({
+            paddingLeft:$('.left-panel').width()
         },300);
     }
 }
@@ -27,4 +33,7 @@ $(function(){
     })
     $('.collapsible.show-first li:eq(0)').addClass('active');
     $('.collapsible.show-first li:eq(0) .collapsible-body').slideDown(200);
+    $('.show-reply-form').click(function(){
+        $(this).parents('.card-content').find('.reply-message').stop(true,true).slideToggle();
+    });
 });
